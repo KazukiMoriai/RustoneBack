31e27979fd3254827ff55aa1c7455ae7aa3d7e10
KazukiMoriai <moriaikazuki@gmail.com>
2025-03-12 13:31:45 +0900

    API route設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,32 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // APIルートを直接ここで登録
+        Route::prefix('api')->middleware('api')->group(function () {
+            Route::post('/photos', [PhotoController::class, 'store']);
+
+        });
     }
 }
