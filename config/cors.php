369b07c0701af48cfe175c17baa270d7c668f16e
KazukiMoriai <moriaikazuki@gmail.com>
2025-03-12 11:44:37 +0900

    CORS設定

--- config/cors.php
+++ config/cors.php
@@ -7,27 +7,15 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:8000',
-        'http://localhost:5173',  // Vite開発サーバーのデフォルトポート
-        'https://moriai.sakura.ne.jp',
-        'https://rust-one-frontend.vercel.app'
-    ],
+    'allowed_origins' => ['https://rust-one-frontend.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => [
-        'Content-Type',
-        'X-Requested-With',
-        'Authorization',
-        'Accept',
-        'X-XSRF-TOKEN'
-    ],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
